cb288632084ec7669670209e1fe261a3d5035657
itm154 <ashrulfahmi@gmail.com>
2026-04-24 00:50:52 +0800

    Add model initialization step

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
+from data import PKData
 from model import PKNODE
+import torch
 import utils
 
 
 def main():
     config = utils.getConfig()
-    print(config)
+
+    # Model initialization, try to use CUDA/GPU if available
+    # Idk if it would work on Windows without additional steps, only tested on Linux
     dim_c = config["settings"]["nn"]["dim_c"]
     dim_V = config["settings"]["nn"]["dim_V"]
+    n_cov = len(config["data"]["columns"]["covariates"])
+    model = PKNODE(dim_c, dim_V, n_cov)
 
-    print(dim_c)
-    print(dim_V)
-
-    model = PKNODE(dim_c, dim_V, 2)
+    device = (
+        torch.accelerator.current_accelerator().type  # pyright: ignore
+        if torch.accelerator.is_available()
+        else "cpu"
+    )
+    model.to(device)
+    print(f"Using {device} device")
     print(model)
 
 
